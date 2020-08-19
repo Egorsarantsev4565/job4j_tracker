30880fe015f07c11143835af5eefc096a64aef58
Егор <egorsarantsev4565@yandex.ru>
2020-08-19 15:27:54 +0300

    2. Scanner и чтение числа из консоли. [#313065]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -10,16 +10,16 @@ public class Matches {
             System.out.println("2");
             System.out.println("3");
             int select = Integer.valueOf(input.nextLine());
-    if (select == 1) {
-        matches = matches - 1;
-        System.out.println(" Осталось спичек - " + matches);
-    } else if (select == 2) {
-        matches = matches - 2;
-        System.out.println(" Осталось спичек - " + matches);
-    } else if (select == 3) {
-        matches = matches - 3;
-        System.out.println(" Осталось спичек - " + matches);
-    }
+            if (select == 1) {
+                matches = matches - 1;
+                System.out.println(" Осталось спичек - " + matches);
+            } else if (select == 2) {
+                matches = matches - 2;
+                System.out.println(" Осталось спичек - " + matches);
+            } else if (select == 3) {
+                matches = matches - 3;
+                System.out.println(" Осталось спичек - " + matches);
+            }
             System.out.println("Второй игрок берет спички : ");
             System.out.println("1");
             System.out.println("2");
