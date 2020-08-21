ec932fa8825b495b87a962665c33c78d549aa425
egorsarantsev4565 <Strelok_4565>
2020-08-21 12:33:43 +0300

    2. Scanner и чтение числа из консоли. [#313065] - 3

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -3,27 +3,30 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         int matches = 11;
+        boolean player = true;
+        boolean player1 = true;
+        boolean player2 = false;
         Scanner input = new Scanner(System.in);
         while (matches > 0) {
-            System.out.println("Первый игрок берет спички : 1 - 2 - 3");
-
+            boolean person = player == true ? player1 : player2;
+            if (player == true) {
+                System.out.println("Первый берет спички : 1 - 2 - 3");
+            } else if (player == false) {
+                System.out.println("Второй игрок берет спички : 1 - 2 - 3");
+            }
             int select = Integer.valueOf(input.nextLine());
             if (select == 1 || select == 2 || select == 3) {
                 matches = matches - select;
-                System.out.println(" Осталось спичек - " + matches);
-            } else if (matches <= 0) {
-                System.out.println("Спичек больше не осталось");
-            }
-            System.out.println("Второй игрок берет спички : 1 - 2 - 3 ");
-            select = Integer.valueOf(input.nextLine());
-            if (select == 1 || select == 2 || select == 3) {
-                matches = matches - select;
-                System.out.println(" Осталось спичек - " + matches);
-            } else if (matches <= 0) {
-                System.out.println("Спичек больше не осталось");
+                if (matches <= 0) {
+                    System.out.println(" Спички закончились. Победитель : " + person );
 
 
+                } System.out.println(" Осталось спичек - " + matches);
+                player = !player;
             }
-        }
+
+            }
+
     }
 }
+
