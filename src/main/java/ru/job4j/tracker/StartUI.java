a117803b91bf66548dcf6a776e65800331e50768
Егор <egorsarantsev4565@yandex.ru>
2020-08-28 15:01:23 +0300

    1. Обеспечить бесперебойную работу приложения Tracker [#313090]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,13 +1,14 @@
 package ru.job4j.tracker;
 public class StartUI {
     private final Output out;
+
     public StartUI(Output out) {
         this.out = out;
     }
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output), new ReplaceAction(output),
@@ -17,19 +18,25 @@ public class StartUI {
         };
         new StartUI(output).init(input, tracker, actions);
     }
+
     private void showMenu(UserAction[] actions) {
         out.println("Menu.");
         for (int index = 0; index < actions.length; index++) {
             out.println(index + ". " + actions[index].name());
         }
     }
-    public void init (Input input, Tracker tracker, UserAction[]actions) {
-            boolean run = true;
-            while (run) {
-                this.showMenu(actions);
-                int select = input.askInt("Select: ");
-                UserAction action = actions[select];
-                run = action.execute(input, tracker);
+
+    public void init(Input input, Tracker tracker, UserAction[] actions) {
+        boolean run = true;
+        while (run) {
+            this.showMenu(actions);
+            int select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+                continue;
             }
+            UserAction action = actions[select];
+            run = action.execute(input, tracker);
         }
     }
+}
