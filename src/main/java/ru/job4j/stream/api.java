a4500661741bbe2bb3827100c26123789d36ed38
Егор <egorsarantsev4565@yandex.ru>
2020-11-26 13:14:53 +0300

    0. Stream API [#312895]

---
+++ src/main/java/ru/job4j/stream/api.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class api {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(-1);
+        numbers.add(2);
+        numbers.add(-2);
+        numbers.add(3);
+        numbers.add(-3);
+       List<Integer> plusNum = numbers.stream().filter(i -> i >= 0).collect(Collectors.toList());
+        plusNum.forEach(System.out::println);
+    }
+}
