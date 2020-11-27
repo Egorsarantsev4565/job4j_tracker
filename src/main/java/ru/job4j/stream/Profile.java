26fd36051dbfae8d9586b64a84b2f77c97b28399
Егор <egorsarantsev4565@yandex.ru>
2020-11-27 14:18:20 +0300

    2. Список адресов [#312897]

---
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+public class Profile {
+    private Address address;
+
+   public Profile(String city, String street, int home, int apartment) {
+       address = new Address(city, street, home, apartment);
+   }
+public Address getAddress() {
+       return address;
+}
+}
+
---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.stream;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ProfilesTest {
+    private List<Profile> profiles = new ArrayList<>();
+    @Before
+    public void setUp() {
+  profiles.add(new Profile("Nizhniy Novgorod", "Karla Marksa", 2, 48));
+  profiles.add(new Profile("Kaliningrad", "Lenina", 5, 89));
+  profiles.add(new Profile("Nizhniy Novgorod", "Akimova", 37, 73));
+  profiles.add(new Profile("Moscow", "Lubyanka", 4, 50));
+    }
+    @Test
+    public void tourists() {
+        List<Address> rsl = new Profiles().collect(profiles);
+        List<Address> expected = List.of(
+                new Address("Nizhniy Novgorod", "Karla Marksa", 2, 48),
+                new Address("Kaliningrad", "Lenina", 5, 89),
+                new Address("Nizhniy Novgorod", "Akimova", 37, 73),
+                new Address("Moscow", "Lubyanka", 4, 50)
+
+        );
+        assertThat(rsl, is(expected));
+    }
+    }
+
